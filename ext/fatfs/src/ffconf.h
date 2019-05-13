45615e7baf404cae1bbca7683d00276aad6f6b6b
bvernoux <bvernoux@gmail.com>
2019-05-13 19:44:59 +0200

    ext/fatfs/src/ffconf.h restore long file name support (like in previous HydraFW ChibiOS version stable_17.6.x)

--- ext/fatfs/src/ffconf.h
+++ ext/fatfs/src/ffconf.h
@@ -97,7 +97,7 @@
 */
 
 
-#define FF_USE_LFN		0
+#define FF_USE_LFN		3
 #define FF_MAX_LFN		255
 /* The FF_USE_LFN switches the support for LFN (long file name).
 /
@@ -219,7 +219,7 @@
 #define FF_FS_NORTC		0
 #define FF_NORTC_MON	5
 #define FF_NORTC_MDAY	1
-#define FF_NORTC_YEAR	2017
+#define FF_NORTC_YEAR	2019
 /* The option FF_FS_NORTC switches timestamp functiton. If the system does not have
 /  any RTC function or valid timestamp is not needed, set FF_FS_NORTC = 1 to disable
 /  the timestamp function. All objects modified by FatFs will have a fixed timestamp
